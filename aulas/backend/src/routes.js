8376712962b1acc5d42bef41b7116019ec737db7
Lucas Pires <lucaspiressimao@gmail.com>
2020-03-27 14:00:41 -0300

    Criando listagem ONGs

--- aulas/backend/src/routes.js
+++ aulas/backend/src/routes.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const routes = express.Router()
 const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+
 
 module.exports = routes
