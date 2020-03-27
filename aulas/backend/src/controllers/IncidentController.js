790267e8d3228fc00f597449e45143b82c59128c
Lucas Pires <lucaspiressimao@gmail.com>
2020-03-27 14:42:50 -0300

    Adicionando o Incidente

--- aulas/backend/src/controllers/IncidentController.js
+++ aulas/backend/src/controllers/IncidentController.js
@@ -3,7 +3,17 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(request, response) {
-        const incidents = await connection('incidents').select('*')
+        const { page = 1 } = request.query
+
+        const [count] = await connection('incidents').count()
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf'])
+
+        response.header('X-Total-Count', count['count(*)'])
         return response.json(incidents)
     },
     async create(request, response) {
@@ -26,7 +36,7 @@ module.exports = {
         const incidents = await connection('incidents').where('id', id).select('ong_id').first()
 
         if (incidents.ong_id !== ong_id) {
-            return response.status(401).json({error: 'Operation not permitted.'})
+            return response.status(401).json({ error: 'Operation not permitted.' })
         }
 
         await connection('incidents').where('id', id).delete()
